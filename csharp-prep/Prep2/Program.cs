3503f26493c49c3b0cabb97b697dc35e807909cc
IK3CHI <ikechigreat@gmail.com>
2024-06-15 22:19:42 +0100

    Program 1,2,3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage:  ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        // string letter;
+
+        if (grade >= 90 ) 
+            {
+                string letter = "A";
+                
+                Console.WriteLine($"Your letter grade is, {letter}");
+                  if (grade >= 90) 
+                 {
+                    Console.WriteLine($"Congratulations, You passed");
+                 }
+            }
+        else if (grade >= 80)
+            {
+                string letter = "B";
+                 Console.WriteLine($"Your letter grade is, {letter}");
+                   if (grade >= 80) 
+                 {
+                    Console.WriteLine($"Congratulations, You passed");
+                 }
+            }
+        else if (grade >= 70)
+            {
+                string letter = "C";
+                 Console.WriteLine($"Your letter grade is, {letter}");
+                 if (grade >= 70) 
+                 {
+                    Console.WriteLine($"Congratulations, You passed");
+                 }
+            }
+        else if (grade >= 60)
+            {
+                 string letter = "D";
+                 Console.WriteLine($"Your letter grade is, {letter}");
+                   if (grade >= 60) 
+                        {
+                         Console.WriteLine($"Please do better next time, You didnt meet the cut off point");
+                        }
+            }
+        else if (grade < 60)
+            {
+                string letter = "F";
+                 Console.WriteLine($"Your letter grade is, {letter}");
+                  if (grade < 60) 
+                        {
+                         Console.WriteLine($"Please do better next time, You didnt meet the cut off point");
+                        }
+            }
+
+        else 
+            {
+                Console.WriteLine("You have reached the end of this program!");
+            }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        bool playAgain = true;
+
+        while (playAgain) 
+        {
+            PlayGame();
+             playAgain = AskToPlayAgain();
+        }
+
+        Console.WriteLine("Thanks for playing again, Goodbye!");
+
+        static void PlayGame()
+        {
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101); //generates a random number between 1 and 100
+            int userGuess = 0;
+
+            Console.WriteLine("Welcome to the Guess number Game");
+            Console.WriteLine("I'm thinking of a number between 1 and 100, can you guess it?");
+
+            while (userGuess != magicNumber)
+            {
+                Console.Write("Enter your guess");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out userGuess))
+                {
+                    if (userGuess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    }
+                    else if (userGuess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Congratulations! You guessed the magic number {magicNumber}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[Please enter a valid number");
+                }
+            }
+        }
+        static bool AskToPlayAgain()
+        {
+            Console.WriteLine("Do you want to play again (yes/no)");
+            string response = Console.ReadLine().ToLower();
+            return response == "yes" || response == "y";
+        }
 }
