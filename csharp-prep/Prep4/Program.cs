be8b58eb5628c5b501b4c42e943caca81e44e0b1
IK3CHI <ikechigreat@gmail.com>
2024-06-16 15:08:27 +0100

    Modified program 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       Compute();
+    }
+
+    static void Compute()
+    {
+        List<double> numbers = new List<double>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            if (num == 0)
+            {
+                break;
+            }
+
+            numbers.Add(num);
+        }
+
+        //compute the sum of the numbers
+        double totalSum = 0;
+        foreach (double number in numbers)
+        {
+            totalSum += number;
+        }
+        
+        //compute the average of the number
+        double average = 0;
+        if (numbers.Count > 0)
+        {
+            average = totalSum / numbers.Count;
+        }
+
+        //Print the sum
+        Console.WriteLine($"The sum is: {totalSum}");
+
+        //find the maximum number in the list
+        double maxNumber = double.MinValue;
+        foreach (double number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {totalSum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
