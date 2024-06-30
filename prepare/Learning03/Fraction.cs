1cbcc830518dee27f26550e21faeabdfc4c9b4c4
IK3CHI <ikechigreat@gmail.com>
2024-06-30 19:34:12 +0100

    All Fraction instance working as expected

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,64 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _buttom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _buttom = 1;
+
+        Console.WriteLine($"{_top} / {_buttom}");
+    }
+
+    public Fraction(int wholenumner)
+    {
+       _top = wholenumner;
+       _buttom = 1;
+
+        Console.WriteLine($"{_top} / {_buttom}");
+    }
+
+    public Fraction(int up, int down)
+    {
+        _top = up;
+        _buttom = down;
+
+        Console.WriteLine($"{_top} / {_buttom}");
+    }
+
+    public int GetTop(int numerator)
+    {
+        return numerator;
+    }
+
+    public void SetTop(int numerator)
+    {
+        _top = numerator;
+    }
+    
+      public int GetButtom()
+    {
+        return _buttom;
+    }
+
+    public void SetButtom(int denominator)
+    {
+        _buttom = denominator;
+    }
+
+    public string GetFractionString(string numerator, string denominator)
+    {
+        string _numerator = numerator;
+        string _denominator = denominator;
+
+        return $"{_numerator} / {_denominator}";
+    }
+
+    public static double GetDecimalValue(int _top, int _buttom)
+    {
+        return (double) _top / _buttom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        int numerator = 3;
+        int denominator = 4;
+
+
+        Fraction test = new Fraction();
+        Fraction test1 = new Fraction(numerator);
+        Fraction test2 = new Fraction(numerator,denominator);
+
+        double decimalValue = Fraction.GetDecimalValue(1,2);
+        int _numerator = test1.GetTop(numerator);
+
+        Console.WriteLine($"{decimalValue}");
+        Console.WriteLine($"{_numerator}");
     }
 }
